c9b8f0802313d29b3e303401b118747b1b63faa6
samiularafatmahi <mdmahi5035@gmail.com>
2025-03-01 11:50:27 -0600

    clear history commit

--- script/scricpt.js
+++ script/scricpt.js
@@ -21,7 +21,7 @@ for (let i = 0; i < buttons.length; i++) {
         }
         const container = document.createElement('p');
         container.textContent = 'You have completed the task Fix video loading issue at 12:10:20 AM'
-        container.classList.add('bg-gray-100', 'p-3', 'rounded-lg', 'mt-8');
+        container.classList.add('bg-indigo-50', 'p-3', 'rounded-lg', 'mt-8');
         const newText = document.getElementById('clear-container');
         if (newText) {
             newText.appendChild(container);
@@ -29,6 +29,18 @@ for (let i = 0; i < buttons.length; i++) {
         else {
             alert('Do not excess');
         }
-
+        document.getElementById('clear-btn').addEventListener('click', function () {
+            container.remove();
+        })
     })
 };
+document.getElementById('btn-6').addEventListener('click', function (event) {
+    event.preventDefault();
+    alert("Congrats!! You have completed all the current task");
+});
+document.getElementById('nav-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+    console.log(random)
+})
